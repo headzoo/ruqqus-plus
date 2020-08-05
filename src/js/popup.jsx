363c63b3ec1af622cd6bd921ea11cc464330f52a
headzoo <sean@headzoo.io>
2020-08-05 16:16:04 -0400

    Popup fixes

--- src/js/popup.jsx
+++ src/js/popup.jsx
@@ -49,35 +49,38 @@ class App extends React.Component {
             </div>
           ) : (
             <div className="mb-3 d-flex justify-content-between align-items-center">
-              <div className="d-flex justify-content-between align-items-center">
-                <img src={user.profile_url} alt="Avatar" className="avatar mr-2" />
-                <div className="d-flex flex-column">
-                  <span className="username mr-2">{user.username}</span>
-                  <div>
+              {user ? (
+                <>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <img src={user.profile_url} alt="Avatar" className="avatar mr-2" />
+                    <div className="d-flex flex-column">
+                      <span className="username mr-2">{user.username}</span>
+                      <div>
                     <span id="container-authed-rep" className="rep">
                       {parseInt(user.post_rep, 10) + parseInt(user.comment_rep, 10)}&nbsp;Rep
                     </span>
+                      </div>
+                    </div>
                   </div>
+                  <a
+                    className="position-relative"
+                    href="https://ruqqus.com/notifications"
+                    target="_blank"
+                    onClick={this.handleUnreadClick}
+                  >
+                    <i className={`fas fa-bell ${unread > 0 ? 'text-danger' : ''}`} />
+                    {unread > 0 && (
+                      <span className="badge-count font-weight-bolder">{unread}</span>
+                    )}
+                  </a>
+                </>
+              ) : (
+                <div>
+                  <a href="https://ruqqus.com" target="_blank">
+                    Log into Ruqqus
+                  </a>
                 </div>
-              </div>
-              <a
-                className="position-relative"
-                href="https://ruqqus.com/notifications"
-                target="_blank"
-                onClick={this.handleUnreadClick}
-              >
-                <i className={`fas fa-bell ${unread > 0 ? 'text-danger' : ''}`} />
-                {unread > 0 && (
-                  <span className="badge-count font-weight-bolder">{unread}</span>
-                )}
-              </a>
-            </div>
-          )}
-          {!user && (
-            <div className="mb-2 hidden">
-              <a href="https://ruqqus.com" target="_blank">
-                Log into ruqqus
-              </a>
+              )}
             </div>
           )}
           <div className="btn-group w-100">
