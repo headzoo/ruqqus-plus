1b6ce00ef4cef5da9eaa8c523b5a4ff6cad1ed6b
headzoo <sean@headzoo.io>
2020-08-21 21:00:39 -0400

    PreviewPostModule handles comments

--- src/js/modules/PreviewPostModule.js
+++ src/js/modules/PreviewPostModule.js
@@ -1,6 +1,5 @@
 import marked from 'marked';
 import Module from './Module';
-import { isDarkMode } from '../utils/ruqqus';
 
 /**
  * Adds a preview pane to the post submit page
@@ -56,11 +55,38 @@ export default class PreviewPostModule extends Module {
    * have access to the ruqqus `window` object.
    */
   execContentContext = () => {
-    if (document.location.pathname !== '/submit') {
-      return;
+    this.listen('rp.change', this.wireup);
+
+    this.onDOMReady(this.wireup);
+  };
+
+  /**
+   *
+   */
+  wireup = () => {
+    const comments = document.querySelector('.comment-section');
+    if (comments) {
+      this.html.querySelectorEach(comments, '.comment-actions span', (span) => {
+        if (span.textContent === 'Reply') {
+          const link = span.closest('a');
+          if (link) {
+            const actions = link.closest('.comment-actions');
+            if (actions) {
+              const commentId = actions.getAttribute('id').replace('comment-', '').replace('-actions', '');
+              link.setAttribute('data-rp-preview-post-reply-id', commentId);
+            }
+            link.addEventListener('click', this.handleCommentLinkClick, false);
+          }
+        }
+      });
+
+      const topTextarea = document.querySelector('#main-content-col .comment-box');
+      if (topTextarea) {
+        topTextarea.addEventListener('click', this.handleCommentLinkClick, false);
+      }
     }
 
-    this.onDOMReady(() => {
+    if (document.location.pathname === '/submit') {
       const label   = document.querySelector('label[for="body"]');
       this.textarea = document.getElementById('post-text');
       this.link     = this.html.createElement('a', {
@@ -69,17 +95,17 @@ export default class PreviewPostModule extends Module {
         'href':  'javascript:void(0)', // eslint-disable-line
         'html':  '<i class="fas fa-eye"></i> Preview',
         'on':    {
-          'click': this.handleLinkClick
+          'click': this.handlePostLinkClick
         }
       });
       label.parentNode.insertBefore(this.link, label);
-    });
+    }
   };
 
   /**
    * @param {Event} e
    */
-  handleLinkClick = (e) => {
+  handlePostLinkClick = (e) => {
     e.preventDefault();
 
     if (!this.isPreviewing) {
@@ -90,9 +116,6 @@ export default class PreviewPostModule extends Module {
         'class': 'rp-preview-post-container rounded',
         'html':  html
       });
-      if (isDarkMode()) {
-        div.classList.add('rp-preview-post-container-dark');
-      }
 
       this.textarea.style.display = 'none';
       container.appendChild(div);
@@ -105,5 +128,70 @@ export default class PreviewPostModule extends Module {
       this.textarea.style.display = 'block';
       this.html.setHTML(this.link, '<i class="fas fa-eye"></i> Preview');
     }
+  };
+
+  /**
+   * @param {*} e
+   */
+  handleCommentLinkClick = (e) => {
+    const { currentTarget } = e;
+    e.preventDefault();
+    e.stopPropagation();
+
+    setTimeout(() => {
+      let textarea;
+      let commentId;
+      if (currentTarget.tagName === 'TEXTAREA') {
+        textarea = currentTarget;
+      } else {
+        commentId = currentTarget.getAttribute('data-rp-preview-post-reply-id');
+        textarea  = document.querySelector(`#reply-to-${commentId} .comment-box`);
+      }
+
+      if (textarea) {
+        const nextSibling = textarea.nextElementSibling;
+        if (textarea && nextSibling.classList.contains('comment-format')) {
+          this.html.query('.rp-preview-post-format').remove();
+          const format = this.html.createElement('small', {
+            'class': 'format rp-preview-post-format rp-pointer',
+            'title': 'Preview',
+            'html':  '<i class="fas fa-eye"></i> Preview'
+          });
+
+          /**
+           * @param {*} ee
+           */
+          const handleClick = (ee) => {
+            ee.preventDefault();
+            ee.stopPropagation();
+
+            if (!textarea.getAttribute('rp-preview-post-previewing')) {
+              const html = marked(textarea.value);
+              const div  = this.html.createElement('div', {
+                'class': 'rp-preview-post-container rounded',
+                'html':  html
+              });
+              textarea.style.display = 'none';
+              this.html.insertAfter(textarea, div);
+              this.html.setHTML(format, '<i class="fas fa-edit"></i> Edit');
+              format.setAttribute('title', 'Edit');
+
+              textarea.setAttribute('rp-preview-post-previewing', 'true');
+            } else {
+              textarea.closest('form').querySelector('.rp-preview-post-container').remove();
+              textarea.style.display = 'block';
+              this.html.setHTML(format, '<i class="fas fa-eye"></i> Preview');
+              format.setAttribute('title', 'Preview');
+
+              textarea.removeAttribute('rp-preview-post-previewing');
+            }
+          };
+
+          format.addEventListener('click', handleClick, false);
+          const formats = textarea.nextElementSibling.querySelectorAll('.format');
+          this.html.insertAfter(formats[formats.length - 1], format);
+        }
+      }
+    }, 100);
   };
 }
